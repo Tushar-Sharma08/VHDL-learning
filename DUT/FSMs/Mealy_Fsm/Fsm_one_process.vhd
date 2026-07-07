089b2a78c5ce4efafff0778c8506529a37c50dea
Tushar Sharma <tusharsharma2005t@gmail.com>
2026-07-07 18:13:33 +0530

    creat fsm_one_process.vhd

---
+++ DUT/FSMs/Mealy_Fsm/Fsm_one_process.vhd
@@ -0,0 +1,47 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity FSM_1 is
+    Port ( clk,rst,din : in STD_LOGIC;
+           dout : out STD_LOGIC);
+end FSM_1;
+
+architecture Behavioral of FSM_1 is
+
+type state_type is (s0,s1);
+signal state : state_type := s0;
+
+begin
+
+rst_next_state_OP_logic: process(clk)
+begin
+    if rising_edge(clk) then
+        if rst = '1' then
+            state <= s0;
+        else
+            case(state)is
+                when s0 => 
+                    if(din='1') then
+                        state <= s1;
+                        dout <= '0';
+                    else
+                        state <= s0;
+                        dout <= '1';
+                    end if;
+                when s1=>
+                    if din = '1' then
+                        state <= s0;
+                        dout <= '1';
+                    else
+                        state <= s1;
+                        dout <= '0';
+                    end if;
+                when others =>
+                    state <= s0;
+                    dout <= '0'; 
+             end case;
+           end if;
+        end if;
+end process;
+end behavioral;          
